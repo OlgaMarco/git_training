ec1023095bef5456be46da518340d3ae07080ba7
OlgaMarco <olga.marco.7e4@itb.cat>
2020-11-05 11:46:40 +0100

    JEJE

---
+++ If.java
@@ -0,0 +1,9 @@
+public class If{
+	public static void main(String[] args){
+		String name = "Sara";
+		if(name == "Manolo") {
+			System.out.println("Jeje"); }
+		else {
+			System.out.println("Jaja"); }
+	}
+}
---
+++ switch2.java
@@ -0,0 +1,13 @@
+public class switch2{
+        public static void main(String[] args){
+                String word = "Sara";
+                switch (word){
+                case "Manolo":
+                        System.out.println("Jeje");
+                        break;
+                case "Sara":
+                        System.out.println("Jaja");
+                        break;
+		}
+        }
+}
